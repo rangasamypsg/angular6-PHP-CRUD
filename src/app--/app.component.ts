dc2eccead5f4f0f59fe409f622889b058e7a0655
yuvaraj <yuvaraj@thegang.in>
2018-11-02 12:16:04 +0530

    Angular6 PHP Crud

---
+++ src/app--/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Car } from './car';
+import { CarService } from './car.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  cars: Car[];
+  error = '';
+  success = '';
+
+  car = new Car('', 0);
+
+  constructor(private carService: CarService) {
+  }
+
+  ngOnInit() {
+    this.getCars();
+  }
+
+  getCars(): void {
+    this.carService.getAll().subscribe(
+      (res: Car[]) => {
+        this.cars = res;
+      },
+      (err) => {
+        this.error = err;
+      }
+    );
+  }
+
+}
--- src/app--/app.module.ts
+++ src/app--/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,8 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
